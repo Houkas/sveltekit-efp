9f4ff2f2cf0fa45559a671d1a423cb5682f61e08
Hugo RICHARD <hugor33@outlook.fr>
2022-05-27 17:31:21 +0200

    Continue l'intégration homepage

---
+++ src/components/Carousel/carousel.svelte
@@ -0,0 +1,36 @@
+<script>
+  import { fade, slide } from "svelte/transition";
+
+  const carouselPhotos = [
+    "/bg-home-efp.jpg",
+    "/depot-efp.jpg",
+    "/logo-efp.jpg",
+  ];
+
+  let index = 0;
+
+  const next = () => {
+    index = (index + 1) % carouselPhotos.length;
+  };
+  const prev = () => {
+    index = index != 0 ? index - 1 : index + carouselPhotos.length - 1;
+  };
+</script>
+
+{#each [carouselPhotos[index]] as src (index)}
+  <div class="container-carousel absolute">
+    <img transition:fade {src} alt="" class="max-w-[1095px] z-5" />
+
+    <div class=" flex flex-row items-end justify-between">
+      <button class=" left-0 top-0 h-5 w-5" on:click={prev}>
+        <img class="h-5 w-5 " src="/arrow_left.svg" alt="précédent" />
+      </button>
+      <button class=" right-0 top-0 h-5 w-5" on:click={next}>
+        <img class="h-5 w-5 " src="/arrow_right.svg" alt="suivant" />
+      </button>
+    </div>
+  </div>
+{/each}
+
+<style>
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
-  import { fade, draw, blur, slide, fly } from "svelte/transition";
-  import {
-    quintOut,
-    quadInOut,
-    quadIn,
-    quadOut,
-    sineIn,
-    cubicOut,
-    elasticInOut,
-    expoInOut,
-  } from "svelte/easing";
+  import { fade, slide } from "svelte/transition";
+  import { expoInOut } from "svelte/easing";
+  import Carousel from "../components/Carousel/carousel.svelte";
   $: y = 0;
   $: width = 0;
   $: height = 0;
@@ -35,8 +27,8 @@
 <div class="fixed left-0 top-10 z-20 text-red-500">Height : {height}</div>
 <div class="fixed left-0 top-20 z-20 text-red-500">Width : {width}</div>
 
-<div transition:fade class="content-container" data-scroll-container>
-  <section class="container-content " data-scroll-section>
+<div transition:fade class="content-container">
+  <section class="container-content ">
     <div class="content-video mx-auto">
       <video autoplay loop muted>
         <source src="/intro2.mp4" type="video/mp4" />
@@ -51,7 +43,7 @@
     </div>
   </section>
 
-  <section class="container-content" data-scroll-section>
+  <section class="container-content">
     <div
       class="content-desc dashed dashed-top min-h-screen mx-auto content-box"
     >
@@ -67,24 +59,27 @@
         car il est n'est pas dans le dom tant que scroll y < 400
       -->
       {#if (y > 400 && width > 1280) || (y >= 0 && width < 640)}
-      <div transition:slide={{ delay: 250, duration: 1000, easing: expoInOut }} class="grey-rec">
-        <h2>Lorem ipsum ipsum</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut ante
-          ante. Nam semper, mauris vitae aliquam facilisis, nulla mauris
-          porttitor dui, sodales faucibus ligula felis quis augue. Praesent
-          lobortis aliquam tempor. Vestibulum quis vestibulum ex. Sed eu
-          malesuada metus. Class aptent taciti sociosqu.<br />
-        </p>
-        <p>
-          Ut quis elit iaculis, porta arcu et, aliquam sapien. Nulla blandit
-          ante tincidunt ex mollis aliquet. Integer et pretium felis. In varius
-          finibus eros ut rutrum. Pellentesque mauris libero, scelerisque ut
-          ornare id, aliquam nec ex. In vehicula pellentesque vehicula. Aliquam
-          consequat erat eu elit egestas, quis malesuada ligula consequat.
-          Aenean sit amet posuere massa.
-        </p>
-      </div>
+        <div
+          transition:slide={{ delay: 250, duration: 1000, easing: expoInOut }}
+          class="grey-rec"
+        >
+          <h2>Lorem ipsum ipsum</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut
+            ante ante. Nam semper, mauris vitae aliquam facilisis, nulla mauris
+            porttitor dui, sodales faucibus ligula felis quis augue. Praesent
+            lobortis aliquam tempor. Vestibulum quis vestibulum ex. Sed eu
+            malesuada metus. Class aptent taciti sociosqu.<br />
+          </p>
+          <p>
+            Ut quis elit iaculis, porta arcu et, aliquam sapien. Nulla blandit
+            ante tincidunt ex mollis aliquet. Integer et pretium felis. In
+            varius finibus eros ut rutrum. Pellentesque mauris libero,
+            scelerisque ut ornare id, aliquam nec ex. In vehicula pellentesque
+            vehicula. Aliquam consequat erat eu elit egestas, quis malesuada
+            ligula consequat. Aenean sit amet posuere massa.
+          </p>
+        </div>
       {/if}
 
       <img
@@ -92,36 +87,39 @@
         src="/depot-efp.jpg"
         alt="Dépot EFP à Saveterre de Guyenne"
       />
-      <span class="indic">Qui sommes-nous ?</span>
+      <span class="indic-desc">Qui sommes-nous ?</span>
     </div>
   </section>
 
-  <section class="container-content" data-scroll-section>
-    <div class="dashed dashed-top min-h-screen mx-auto content-box">
-      <div class="content-realisation flex flex-row items-center">
-        <div class="h-screen w-1/3 flex flex-row items-center">
-          <a href="/realisations" class="z-10">
-            {#if y >= 1600 && width > 1280}
-              <div
-                transition:fade
-                class="cicle-gradient absolute left-[40px] block content-[''] bg-[url('/circle_degrade.svg')] w-[40vh] h-[40vh] z-[-1] mt-[-20vh]"
-              />
-              <h2
-                class={y >= 1600 && width > 1280 ? "anim-txt-lft" : "opacity-0"}
-                out:fade
-              >
-                Réalisations
-              </h2>
-            {/if}
-          </a>
-        </div>
-        <div class="h-screen bg-gray-100 w-2/3" />
+  <section class="container-content">
+    <div
+      class="dashed dashed-top min-h-screen mx-auto content-box content-realisation flex flex-row items-center"
+    >
+      <div class="h-screen w-1/3 flex flex-row items-center">
+        <a href="/realisations" class="z-10">
+          {#if y >= 1600 && width > 1280}
+            <div
+              transition:fade
+              class="cicle-gradient absolute left-[40px] block content-[''] bg-[url('/circle_degrade.svg')] w-[40vh] h-[40vh] z-[-1] mt-[-20vh]"
+            />
+            <h2
+              class={y >= 1600 && width > 1280 ? "anim-txt-lft" : "opacity-0"}
+              out:fade
+            >
+              Réalisations
+            </h2>
+          {/if}
+        </a>
       </div>
+      <div class="h-screen w-2/3 flex flex-row items-center">
+        <Carousel />
+      </div>
+      <span class="indic-rea">Réalisations</span>
     </div>
   </section>
 
-  <section class="container-content" data-scroll-section>
-    <div class="dashed dashed-top  min-h-screen mx-auto content-box" />
+  <section class="container-content">
+    <div class="dashed dashed-top min-h-screen mx-auto content-box" />
   </section>
 </div>
 
