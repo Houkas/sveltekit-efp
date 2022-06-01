18139dcdc16f83e45f6fc014c6e9b934da0b50ca
Hugo RICHARD <hugor33@outlook.fr>
2022-06-01 22:29:57 +0200

    css scrollbar on __layout.svelte

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -14,4 +14,20 @@
 <Footer />
 
 <style lang="scss">
+
+  :global::-webkit-scrollbar {
+    width: 8px;
+  }
+  :global::-webkit-scrollbar-thumb {
+    background-color: $vertFonce;
+    border-radius: 5px;
+  }
+  :global::-webkit-scrollbar-track{
+    margin-top: -3px;
+    margin-bottom: -3px;
+  }
+  :global::-webkit-scrollbar-track-piece{
+    border-radius: 5px;
+    background-color:$vertGris;
+  }
 </style>
