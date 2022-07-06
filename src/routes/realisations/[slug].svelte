647fee5672b2f7211a5d6e7397df5f3e409b21a2
Hugo RICHARD <hugor33@outlook.fr>
2022-07-07 00:13:18 +0200

    integration realisation page

--- src/routes/realisations/[slug].svelte
+++ src/routes/realisations/[slug].svelte
@@ -3,57 +3,139 @@
   import { fetchStrapiRealisation } from "../../shared/strapi_service";
   import type { Realisation } from "../../types/realisation";
   import type { Load } from "@sveltejs/kit";
+  import { urlStrapiEfpDEV } from "../../shared/config";
 
   //Pour loader de la data, ici consumer le store, la function load execute le code sur le server et sur le client.
-  export const load:Load = async({ params }) =>{
+  export const load: Load = async ({ params }) => {
     const slug = params.slug;
     const realisation: Realisation = await fetchStrapiRealisation(slug);
-    if(realisation){
+    if (realisation) {
       return {
-      props: {
-        realisation: realisation,
-      },
-    };
+        props: {
+          realisation: realisation,
+        },
+      };
     } else {
       throw new Error();
     }
-    
-  }
+  };
 </script>
 
 <script lang="ts">
-  export let realisation:Realisation;
+  export let realisation: Realisation;
 </script>
 
 <svelte:head>
   <title>Realisation</title>
   {#if realisation}
-    <meta name="description" content={realisation.attributes.meta_description} />
-    {:else}
-    <meta name="description" content="Réalisation travaux de peinture entreprise EFP" />
+    <meta
+      name="description"
+      content={realisation.attributes.meta_description}
+    />
+  {:else}
+    <meta
+      name="description"
+      content="Réalisation travaux de peinture entreprise EFP"
+    />
   {/if}
-
 </svelte:head>
 
-<div
-  class="realisation container mx-auto mt-7 bg-gray-200 min-h-screen"
->
-  <section class=" p-4 border-2 border-solid border-stone-400 w-full">
-    {#if realisation}
-      <h1>{realisation.attributes.title}</h1>
-      {@html realisation.attributes.body}
-      {:else}
-      <p>Chargement du contenu...</p>
-    {/if}
-  </section>
-</div>
+<section class="container-content mt-[70px]">
+  <div class="dashed dashed-top min-h-screen mx-auto content-box flex flex-col">
+    <div class=" flex flex-col items-center">
+      <div class="underline" />
+      <h1
+        class="mt-[30px] text-center uppercase font-bold tracking-widest text-[50px] text-[#26FFF4] z-10"
+      >
+        {realisation.attributes.title}
+      </h1>
+      <div class="flex flex-row">
+        <div
+          class="cicle-gradient  block content-[''] bg-[url('/circle_degrade.svg')] w-[20px] h-[20px] z-0 m-1"
+        />
+        <div
+          class="cicle-gradient  block content-[''] bg-[url('/circle_degrade.svg')] w-[20px] h-[20px] z-0 m-1"
+        />
+        <div
+          class="cicle-gradient  block content-[''] bg-[url('/circle_degrade.svg')] w-[20px] h-[20px] z-0 m-1"
+        />
+      </div>
+      <img
+      src={urlStrapiEfpDEV +
+        realisation.attributes.miniature.data.attributes.url}
+      alt=""
+      class="object-cover w-[320px] h-[207px]"
+      />
+    </div>
+    <div class="flex flex-row items-center">
+      <div class="realisation container mx-auto bg-gray-200">
+        <section class="p-4 w-full">
+          {#if realisation}
+            <h1>{realisation.attributes.title}</h1>
+            {@html realisation.attributes.body}
+          {:else}
+            <p>Chargement du contenu...</p>
+          {/if}
+        </section>
+      </div>
+    </div>
+  </div>
+</section>
 
 <style lang="scss">
-  .realisation {
-    section {
-      margin-top: 70px;
-    }
+  .content-box {
+    padding: 0 80px 0 80px;
+  }
+  .container-content {
+    width: 100%;
 
+    .underline {
+      margin-top: 45px;
+      content: "";
+      position: absolute;
+      height: 52px;
+      width: 80vh;
+      background-color: $vertFonce;
+      z-index: 1;
+    }
+    .bg-degrade {
+      background: linear-gradient(180deg, #7dfeff 0%, #e0f8bc 100%);
+    }
+  }
+  .dashed-top {
+    background-image: linear-gradient(
+      to right,
+      $vertGris 66%,
+      rgba(255, 255, 255, 0) 0%
+    );
+    background-position: top;
+    background-size: 25px 2px;
+    background-repeat: repeat-x;
+  }
+
+  .dashed::before {
+    background-position: left;
+    width: 2px;
+  }
+
+  .dashed::after {
+    background-position: right;
+    right: 80px;
+    z-index: 5;
+    width: 5px;
+  }
+
+  .dashed::after,
+  .dashed::before {
+    content: "";
+    position: absolute;
+    min-height: 100vh;
+    z-index: 10;
+    background-image: linear-gradient($vertGris 66%, rgba(255, 255, 255, 0) 0%);
+    background-size: 2px 25px;
+    background-repeat: repeat-y;
+  }
+  .realisation {
     :global(.kg-gallery-image img) {
       max-width: 250px;
     }
